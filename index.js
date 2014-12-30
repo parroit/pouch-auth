80c957f246901073a56cbc68ad549b52fb75ef68
andrea parodi <andrea.parodi@ebansoftware.net>
2014-12-30 18:03:58 +0100

    first release - not working

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var querystring = require('querystring');
+var assign = require('object-assign');
+
 function remoteDbOnly(pouch) {
 
     if (pouch.adapter.slice(0, 4) !== 'http') {
@@ -10,13 +13,54 @@ function remoteDbOnly(pouch) {
 
 module.exports = {
 
-    login: function() {
+    login: function(user, password) {
+        remoteDbOnly(this);
+
         var pouch = this;
+        var Promise = pouch.constructor.utils.Promise;
+        var request = pouch.constructor.utils.ajax;
+        var pos = pouch._db_name.lastIndexOf('/');
+        var url = pouch._db_name.slice(0, pos) + '/_session';
 
-        remoteDbOnly(pouch);
+        var options = assign({
+            method: 'POST',
+            url: url,
+            form: {
+                name: user,
+                password: password
+            },
+            headers: {
+                Accept: 'application/json'
+            }
+        }, pouch.__opts.ajax);
 
-        var PouchDB = pouch.constructor;
 
+
+        //console.dir(pouch.constructor)
+        //console.dir(options)
+
+        return new Promise(function(resolve, reject) {
+            request(options, function(error, body, response) {
+                
+                if (error) {
+                    return reject(new Error(error.status + ': ' + error.message));
+                }
+
+                if (response.statusCode !== 200) {
+                    return new Error('response status:' + response.statusCode);
+                }
+
+                var opts = pouch.__opts.ajax = pouch.__opts.ajax || {};
+                var headers = opts.headers || {};
+                
+                pouch.__opts.ajax.headers = assign(headers, {
+                    cookie: response.headers['set-cookie'][0]
+                });
+
+                console.dir(pouch.__opts.ajax);
+                resolve();
+            });
+        });
 
     },
 
--- pouch_auth_test.js
+++ pouch_auth_test.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var chai =require('chai');
+var chai = require('chai');
 var expect = chai.expect;
 chai.should();
 
 
 var pouchAuth = require('./index');
 var PouchDB = require('pouchdb');
-
+PouchDB.debug.enable('*');
 
 describe('pouchAuth', function() {
     it('is defined', function() {
@@ -15,17 +15,23 @@ describe('pouchAuth', function() {
     });
 
     describe('when mounted', function() {
-        var local, remote;
+        var local, remote, remoteUrl, remoteUser, remotePassword;
 
         before(function() {
-            var remoteUrl = process.env.test_remote_url;
-            if (!remoteUrl) {
+            remoteUrl = process.env.test_remote_url;
+            remoteUser = process.env.test_remote_usr;
+            remotePassword = process.env.test_remote_pwd;
+            if (!remoteUrl || !remotePassword || !remoteUser) {
                 throw new Error(
-                    'To run tests, you must setup test_remote_url,'+
+                    'To run tests, you must setup test_remote_url,' +
                     ' test_remote_usr and test_remote_pwd environment variable.'
                 );
             }
-            remote = new PouchDB(remoteUrl);
+            remote = new PouchDB(remoteUrl, {
+                ajax: {
+                    strictSSL: false
+                }
+            });
             local = new PouchDB('test_assets');
             PouchDB.plugin(pouchAuth);
         });
@@ -40,18 +46,37 @@ describe('pouchAuth', function() {
             local.logout.should.be.a('function');
         });
 
-         it('login method is not available on local dbs', function() {
-            expect(function(){
-                local.login();
+        it('login method is not available on local dbs', function() {
+            expect(function() {
                 remote.login();
+                local.login();
+
             }).to.throws(TypeError);
         });
 
-
-         it('logout method is not available on local dbs', function() {
-            expect(function(){
+        it('logout method is not available on local dbs', function() {
+            expect(function() {
                 local.logout();
             }).to.throws(TypeError);
+        });
+
+        it.only('allow to login to remote instances', function(done) {
+            remote.login(remoteUser, remotePassword)
+                .then(function() {
+                    remote.allDocs({
+                            limit: 1
+                    })
+                    .then(function(doc) {
+                        console.dir(doc);
+                        done();
+                    })
+                    .catch(function(err) {
+                        done(new Error(err.message));
+                    });
+
+                })
+                .catch(done);
+
         });
     });
 });
